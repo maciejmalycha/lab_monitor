e9f3218075dd889bbcecf164ea53f9c3d0db2e93
Miłosz Rzeźnikowski <varrlo4@gmail.com>
2014-08-07 15:48:02 +0200

    update

--- src/construct_lab.py
+++ src/construct_lab.py
@@ -3,14 +3,14 @@ import sensors
 import database
 
 def run(size):
-	lab = s.Laboratory()
-	lab_size = size	
+    lab = s.Laboratory()
 
-	for rackid in range(lab_size):
-		if database.ServersDAO().server_list(rackid):
-			lab.add_rack(s.Rack(rackid))
+    for rackid in range(size):
+        lab.add_rack(s.Rack(rackid))
 
-	for rack in lab.racks:
-		serv_list = database.ServersDAO().server_list(rack.id)
-		for serv in serv_list:
-			rack.add_server(s.Server(serv['addr'], s.ESXiHypervisor(serv['hypervisor']), sensors.SSHiLoSensors(serv['addr']), database.SensorsDAO()))
+    for rack in lab.racks:
+        serv_list = database.ServersDAO().server_list(rack.id)
+        for serv in serv_list:
+            rack.add_server(s.Server(serv['addr'], s.ESXiHypervisor(serv['hypervisor']), sensors.SSHiLoSensors(serv['addr']), database.SensorsDAO()))
+
+#czy to wszystko?
