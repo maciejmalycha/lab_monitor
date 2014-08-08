25c6064a4b337513c4768bd199a608fbc7aacb66
Miłosz Rzeźnikowski <varrlo4@gmail.com>
2014-08-08 13:54:24 +0200

    kolejny update construct_laba - wszystko w jednej petli

--- src/construct_lab.py
+++ src/construct_lab.py
@@ -8,10 +8,9 @@ def run(size):
     servers_dao = database.ServersDAO()
 
     for rackid in range(size):
-        lab.add_rack(s.Rack(rackid))
-
-    for rack in lab.racks:
-        serv_list = servers_dao.server_list(rack.id)
+        rack = s.Rack(rackid)
+        lab.add_rack(rack)
+        serv_list = servers_dao.server_list(rackid)
         for serv in serv_list:
             rack.add_server(s.Server(serv['addr'], s.ESXiHypervisor(serv['hypervisor']), sensors.SSHiLoSensors(serv['addr']), sensors_dao))
 
